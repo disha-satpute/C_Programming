227007a20f0da516dbf359e0213ec30dcd7955ac
Disha Satpute <dishasatpute44@gmail.com>
2024-09-16 13:37:44 +0530

    add arithmetic operation

---
+++ Arithmatic/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "../include/math.h"
+
+int main(){
+
+
+    addition(10,20);
+    subtraction(40,20);
+    multiplication(5,2);
+    division(10,2);
+
+
+    return 0;
+}
+
+/*
+*****************************************************************
+g++ -Iinclude -c ./src/math.c -o ./build/math.o
+g++ -Iinclude -c ./src/main.c -o ./build/main.o
+g++ ./build/math.o ./build/main.o -o ./build/result.exe
+
+*******************************************************************
+*/
---
+++ Arithmatic/src/math.c
@@ -0,0 +1,22 @@
+#include "../include/math.h"
+#include <stdio.h>
+
+//function implementation
+void addition(int num1, int num2) {
+    int result = num1 + num2;
+    printf("Addition : %d\n",result);
+}
+
+void subtraction(int num1, int num2) {
+    int result = num1 - num2;
+     printf("Subtraction : %d\n",result);
+}
+void multiplication(int num1, int num2) {
+    int result = num1 * num2;
+     printf("Multiplication : %d\n",result);
+}
+
+void division(int num1, int num2) {
+    int result = num1 / num2;
+      printf("Division : %d\n",result);
+}
