58a60bbe36d2799c5514a7f0f461a413dc633ffb
Disha Satpute <dishasatpute44@gmail.com>
2024-09-16 18:33:39 +0530

    circle(using struct point and int)

--- UserDefined_datatype/src/line.c
+++ UserDefined_datatype/src/line.c
@@ -1,11 +1,11 @@
+#include "../include/point.h"
 #include "../include/line.h"
+
 #include <stdio.h>
 
 void getLine(struct line line1) {
 
-     printf("***Line 1 co-ordinates***\n");
-     
+    printf("***Line 1 Coordinates***\n");
     printf("Line 1 - Point 1 - X: %d, Y: %d\n", line1.pt1.x, line1.pt1.y);
     printf("Line 1 - Point 2 - X: %d, Y: %d\n", line1.pt2.x, line1.pt2.y);
-
 }
--- UserDefined_datatype/src/main.c
+++ UserDefined_datatype/src/main.c
@@ -2,7 +2,7 @@
 
 #include "../include/point.h"
 #include "../include/line.h"
-
+#include "../include/circle.h"
 
 //Entry Point Function : main
 int main()
@@ -12,13 +12,16 @@ int main()
   struct point p1 = {10, 20};
   struct point p2 = {30, 40};
 
-  printf("Point 1 \n");
     getXY(p1, p2);
 
   struct line L1={p1,p2};
 
    getLine(L1);
 
+    struct circle c1 = {p1,25};
+     printf("***Circle***\n");
+    printf("center :%d , %d\n",c1.center.x,c1.center.y);
+    printf("radius :%d",c1.radius);
  return 0;
 }
 
@@ -29,8 +32,9 @@ int main()
 
 g++ -Iinclude -c ./src/point.c -o ./build/point.o
 g++ -Iinclude -c ./src/line.c -o ./build/line.o
+g++ -Iinclude -c ./src/circle.c -o ./build/circle.o
 g++ -Iinclude -c ./src/main.c -o ./build/main.o
-g++ ./build/point.o ./build/line.o  ./build/main.o  -o ./build/output.exe
+g++ ./build/point.o ./build/line.o ./build/circle.o ./build/main.o  -o ./build/output.exe
 
 *********************************************************************************
 */
