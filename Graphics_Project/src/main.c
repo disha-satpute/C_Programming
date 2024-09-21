2d367f80494d086afd580c921f7160e03a9d172f
Disha Satpute <dishasatpute44@gmail.com>
2024-09-21 18:58:48 +0530

    add blocks in graphics

--- Graphics_Project/src/main.c
+++ Graphics_Project/src/main.c
@@ -4,28 +4,36 @@
 int main()
 {
     int gd = DETECT, gm;
-
     // Initialize graphics mode
     initgraph(&gd, &gm, "");
 
     // Set color and draw a line
-    setcolor(RED);
-    line(20, 200, 200,20);
+    setcolor(YELLOW);
 
-    //cleardevice();
-    //bar(100, 100, 200, 200);
-    //bar3d(100, 100, 200, 200, 20, 1);
-    // Pause to view the result
+    //Horizontal
+    line(20, 20, 180,20);
+    line(20, 60, 180,60);
+    line(20, 100, 180,100);
+    line(20, 140, 180,140);
+    line(20, 180, 180,180);
+
+// Vertical
+    line(20, 20, 20,180);
+    line(60, 20, 60,180);
+    line(100, 20, 100,180);
+    line(140, 20, 140,180);
+    line(180, 20, 180,180);
+     /*   line(120, 20, 120,100);
+        line(140, 20, 140,100);
+        line(160, 20, 160,100);
+        line(180, 20, 180,100);*/
 
 
-     int x=100, y=100, r=80;
-    circle(x, y, r);
-
-    rectangle(100,100,300,300);
-    getch();
+     getch();
 
     // Close the graphics mode
     closegraph();
+    
     return 0;
 }
 
