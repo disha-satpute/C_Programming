7b8e0f2344fb9b637fbde91a7872223189617f08
Disha Satpute <dishasatpute44@gmail.com>
2024-09-21 20:19:07 +0530

    smiley face

---
+++ Face/src/face.c
@@ -0,0 +1,38 @@
+
+#include <graphics.h>
+#include <conio.h>
+#include<dos.h> // For getch()
+
+int main()
+{
+    int gd = DETECT, gm;
+
+    // Initialize graphics mode
+    initgraph(&gd, &gm, "");
+
+    //circle
+    setcolor(YELLOW);
+    circle(300,250,200);
+
+    //Eyes
+      circle(230,200,40);
+       circle(380,200,40);
+
+    ellipse(300, 330, 210, 335, 90, 30);
+    ellipse(300, 330, 210, 335, 90, 31);
+    ellipse(300, 330, 210, 335, 90, 32);
+
+
+    getch();
+
+    // Close the graphics mode
+    closegraph();
+    return 0;
+}
+
+
+
+//in detail commands for creating ext
+
+//g++ -I include -c ./src/face.c -o ./build/face.o
+//g++  ./build/face.o  -o ./build/output.exe -l bgi -l gdi32 -l comdlg32 -l uuid -l ole32 -l oleaut32
